c2c4cfd933a5c7182e63378a3e0d804cf52f11d6
marrepirre <111415251+marrepirre@users.noreply.github.com>
2023-12-13 11:17:40 +0100

    Website push exit button

--- scripts/menubarscript.js
+++ scripts/menubarscript.js
@@ -1,8 +1,14 @@
 setTimeout(() => {
     const cartIcon = document.querySelector('#shopping-cart');
     const cartWrapper = document.querySelector('.wrapper');
+    const exitButton = document.querySelector('#exit-cart-button')
 
     cartIcon.addEventListener('click', function () {
+        
+        // Toggle klassen 'open' för att visa/dölja varukorgen
+        cartWrapper.classList.toggle('open');
+    });
+    exitButton.addEventListener('click', function () {
         
         // Toggle klassen 'open' för att visa/dölja varukorgen
         cartWrapper.classList.toggle('open');
